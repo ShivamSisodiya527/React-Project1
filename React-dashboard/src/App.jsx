9e1a3fe7b2f9ce842244f2a2f969e6edf804faea
ShivamSisodiya527 <shivamsisodiya2055@gmail.com>
2024-09-27 23:35:54 +0530

    Initial commit

---
+++ React-dashboard/src/App.jsx
@@ -0,0 +1,29 @@
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Support from "./Pages/Support/Support";
+import TransactionPage from "./Pages/Transaction/Transaction";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/transactions",
+    element: <TransactionPage />,
+  },
+  {
+    path: "/support",
+    element: <Support />,
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
